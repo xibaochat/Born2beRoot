22d7bea21422980a2c2e309c6dca6d436f251738
xibaochat <wmmxxbb@gmail.com>
2021-07-30 11:27:48 +0200

    projet

--- ask_all_change_pw.sh
+++ ask_all_change_pw.sh
@@ -1,14 +1,30 @@
-echo "script to ask user to change password\n"
+#!/bin/bash
+
+
+RED="\033[31m"
+GREEN="\033[32m"
+NC="\033[0m"
+
+echo "script to ask user to change password"
 
 USERNAMES=($(getent passwd | grep /bin/bash | cut -d ':' -f 1))
-MYSELF=($whoami)
-
+MYSELF=$(whoami)
+echo $MYSELF
 for user in ${USERNAMES[*]}
 do
-    if [[ $user != $MYSELF ]]
-    then
+   sudo chage -m 2 -M 30 -W 7 $user
+   if [[ $user != $MYSELF ]]
+   then
         echo $user
+        sudo chage -m 2 -M 30 -W 7 $user
         sudo chage -d 0 $user
-        chage -l $user
-    fi
+        sleep 1
+        sudo chage -l $user
+   fi
 done
+
+echo  $MYSELF " change password"
+sleep 1
+sudo chage -d 0 $MYSELF
+sleep 1
+sudo chage -l $MYSELF
--- monitoring.sh
+++ monitoring.sh
@@ -1,84 +1,86 @@
 #!/bin/bash
 
+
 RED="\033[31m"
 GREEN="\033[32m"
 NC="\033[0m"
 
 
-### broadcast() {
-### 	NOW=$(date +%c)
-### 	echo -n -e "Broadcast message from ${GREEN}$USER${NC}"
-### 	echo " ($NOW)"| tr [:lower:] [:upper:]
-### }
-
 Architecture_and_kernel() {
-	Architecture=$(uname -i)
-	Kernel_v=$(uname -v)
-	more_info=$(uname -a)
-	echo -e "\n    #${GREEN}[Architecture]${NC}: $Architecture" "\n" "    ${GREEN}[kernel version]${NC}: $Kernel_v" "\n     ${GREEN}[more detail]${NC}: $more_info"
+    Architecture=$(uname -a | cut -d " " -f 9)
+    Kernel_v=$(uname -v)
+    more_info=$(uname -a)
+    echo -e "\n    #${GREEN}[Architecture]${NC}: $Architecture" "\n" "    ${GREEN}[kernel version]${NC}: $Kernel_v" "\n     ${GREEN}[more de\
+tail]${NC}: $more_info"
 }
 
 cpu_physcial() {
-	physical_core=$(lscpu | egrep 'Socket' | tr -d " " | cut -d ":" -f 2)
-	echo -e "    #${GREEN}[CPU physical]${NC}: $physical_core"
+    physical_core=$(lscpu | egrep 'Socket' | tr -d " " | cut -d ":" -f 2)
+    echo -e "    #${GREEN}[CPU physical]${NC}: $physical_core"
 }
 
 vcpu() {
-	echo -e -n "    #${GREEN}[vCPU]${NC}: "
-	cat /proc/cpuinfo | grep processor | wc -l
+    echo -e -n "    #${GREEN}[vCPU]${NC}: "
+    cat /proc/cpuinfo | grep processor | wc -l
 }
 
 Memory_usage() {
-	total_m=$(free -m | grep Mem: | grep -o -E '[0-9]+' | head -n 1)
-	used_m=$(free -m | grep Mem: | grep -o -E '[0-9]+' | sed -n -e '2{p;q}' )
-	echo -e  "    #${GREEN}[Memory Usage]${NC}: $used_m/${total_m}MB" "($(( 100*$used_m/$total_m ))%)"
+    total_m=$(free -m | grep Mem: | grep -o -E '[0-9]+' | head -n 1)
+    used_m=$(free -m | grep Mem: | grep -o -E '[0-9]+' | sed -n -e '2{p;q}' )
+    echo -e  "    #${GREEN}[Memory Usage]${NC}: $used_m/${total_m}MB" "($(( 100*$used_m/$total_m ))%)"
 }
+#top function as well, MiB Mem, and used
 
 disk_usage() {
-	total_disk=$(df | grep '^/dev/' | awk '{s+=$2} END {print s/1048576}')
-	used=$(df | grep '^/dev/' | awk '{s+=$3} END {print s/1048576}')
-	used_int=$(df | grep '^/dev/' | awk '{s+=$3} END {print s}')
-	total_m_int=$(df | grep '^/dev/' | awk '{s+=$2} END {print s}')
-	echo -e "    #${GREEN}[Disk Usage]${NC}: ${used}/${total_disk}Gb" "($(( 100*${used_int}/$total_m_int ))%)"
+    total_disk=$(df | grep '^/dev/' | awk '{s+=$2} END {print s/1048576}')
+    used=$(df | grep '^/dev/' | awk '{s+=$3} END {print s/1048576}')
+    used_int=$(df | grep '^/dev/' | awk '{s+=$3} END {print s}')
+    total_m_int=$(df | grep '^/dev/' | awk '{s+=$2} END {print s}')
+    echo -e "    #${GREEN}[Disk Usage]${NC}: ${used}/${total_disk}Gb" "($(( 100*${used_int}/$total_m_int ))%)"
 }
 
 cpu_load() {
-	c_load=$(top -bn 1 |grep "Cpu(s)" | awk '{print 100-$8}')
-	echo -e "    #${GREEN}[CPU load]${NC}: $c_load%"
+    c_load=$(top -bn 1 |grep "Cpu(s)" | awk '{print 100-$8}')
+    echo -e "    #${GREEN}[CPU load]${NC}: $c_load%"
 }
 
 last_reboot() {
-	last_reboot=$(who -b | awk '{print $3 " " $4}')
-# 	last_reboot=$(uptime | perl -ne '/.*up +(?:(\d+) days?,? +)?(\d+):(\d+),.*/; $total=((($1*24+$2)*60+$3)*60);
-# $now=time(); $now-=		 $total; $now=localtime($now); print $now,"\n";')
-
-	# 	echo -e "    #${GREEN}[Last boot]${NC}: $last_reboot"
-	echo -e "    #${GREEN}[Last boot]${NC}: $last_reboot"
-
-	echo -e "    #${GREEN}[LVM use]${NC}: DID NOT KNOW YET"
+    last_reboot=$(who -b | awk '{print $3 " " $4}')
+    nb_partition=$(cat /etc/fstab | grep '^/dev/mapper/' | wc -l)
+    echo -e "    #${GREEN}[Last boot]${NC}: $last_reboot"
+    if [ ${nb_partition} -gt 0 ]
+    then
+        echo  -e "    #${GREEN}[LVM use]${NC}: yes"
+    else
+        echo -e "    #${GREEN}[LVM use]${NC}: no"
+    fi
 }
 
 nb_active_connection() {
-	nb_active_connection=$(ss | grep tcp | wc -l)
-	echo -e "    #${GREEN}[Connexions TCP]${NC}: $nb_active_connection ESTABLISHED"
+    nb_active_connection=$(ss | grep tcp | wc -l)
+    echo -e "    #${GREEN}[Connexions TCP]${NC}: $nb_active_connection ESTABLISHED"
 }
+# netstat -s -t | sed -e '/Tcp:/!d;:l;n;/^\ /!d;bl'| grep "connections established"
+# ss -s | grep estab
+
+
 
 user_log() {
-	nb_user=$(who | wc -l)
-	name=$(who | awk -F" " '{print $1}')
-	echo -e "    #${GREEN}[User log]${NC}: $nb_user $name"
+    nb_user=$(who | wc -l)
+    name=$(who | awk -F" " '{print $1}')
+    echo -e "    #${GREEN}[User log]${NC}: $nb_user $name"
 }
 
 network(){
-	net_int=$(ip -4 -o a | grep -v '1: lo' | head -n 1 | awk '{print $2}')
-	ipv4=$(ip -f inet addr show ${net_int} | grep inet | awk '{print $2}' | cut -d '/' -f 1)
-	mac_add=$(ip -f link addr show ${net_int} | tail -n 1 | awk '{print $2}')
-	echo -e "    #${GREEN}[Network] IP${NC}: $ipv4 ($mac_add)"
+    net_int=$(ip -4 -o a | grep -v '1: lo' | head -n 1 | awk '{print $2}')
+    ipv4=$(ip -f inet addr show ${net_int} | grep inet | awk '{print $2}' | cut -d '/' -f 1)
+    mac_add=$(ip -f link addr show ${net_int} | tail -n 1 | awk '{print $2}')
+    echo -e "    #${GREEN}[Network] IP${NC}: $ipv4 ($mac_add)"
 }
 
 nb_sudo() {
-	nb_sudo=$(grep 'sudo:' /var/log/auth.log | grep 'COMMAND=' | wc -l)
-	echo -e "    #${GREEN}[Sudo]${NC}: ${nb_sudo} cmd"
+    nb_sudo=$(grep 'COMMAND='  /var/log/sudo/sudo.log | wc -l)
+    echo -e "    #${GREEN}[Sudo]${NC}: ${nb_sudo} cmd"
 }
 
 
@@ -93,3 +95,14 @@ nb_active_connection
 user_log
 network
 nb_sudo
+
+
+
+
+#[PHYSICAL CPU]
+#Intel refers to a physical processor as a socket.
+#https://www.perfmatrix.com/physical-cpu-and-logical-cpu/
+#[VCPU]
+#https://webhostinggeeks.com/howto/how-to-display-the-number-of-processors-vcpu-on-linux-vps/
+#[CPU load]
+#https://support.site24x7.com/portal/en/kb/articles/how-is-cpu-utilization-calculated-for-a-linux-server-monitor
